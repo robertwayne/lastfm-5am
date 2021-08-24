5d952fc8ee80b8be377ed9aa642c6619a10f019f
Rob <rob@robwagner.dev>
2021-08-24 19:19:02 -0400

    build: added 'compiler' to create a moz-document compatible export.

---
+++ compile.py
@@ -0,0 +1,23 @@
+import os
+
+
+def compileFile(to, fileName):
+    with open(f'src/{fileName}.css', 'r') as file:
+        name = '' if fileName == 'base' else fileName
+        ident = 'url-prefix' if fileName == 'base' else 'url'
+        to.write(f'\n@-moz-document {ident}("https://www.last.fm/{name}")')
+        to.write('\n{\n')
+        for line in file:
+            to.write(line)
+        to.write('\n}\n')
+
+
+def export():
+    with open('export.css', 'w+') as exportFile:
+        exportFile.truncate()
+        for file in os.listdir('./src'):
+            compileFile(exportFile, file.replace('.css', ''))
+
+
+if __name__ == '__main__':
+    export()
